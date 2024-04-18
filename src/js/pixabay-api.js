f74bb154abe9efd5dad26a67ebd821df6c0c71f7
sashok1987nik <sashok1987nk@ukr.net>
2024-04-18 19:04:53 +0200

    up

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,38 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const loader = document.querySelector('.loader');
+const KEY = 43440045-443c1624e1412379947e54b30;
+const BASE_URL = 'https://pixabay.com/api/';
+
+
+
+
+ export function getImages(QUERY) {
+    const LINK = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true`;
+    return fetch(LINK)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.hits.length === 0) {
+            iziToast.error({
+              message:
+                'Sorry, there are no images matching your search query. Please try again!',
+                backgroundColor: '#ef4040',  
+                titleColor: '#FFFFFF',
+                messageColor: '#FFFFFF',
+                theme: 'dark',
+            });
+        }
+        return data;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { renderMarcup } from "./js/render-functions.js";
+import { getImages } from "./js/pixabay-api.js";
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    nav: true,
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 150,
+  });
+
+  const form = document.querySelector('.form');
+  const gallery = document.querySelector('.gallery');
+  let searchWord = '';
+
+  form.addEventListener('submit', workSub);
+
+  
+
