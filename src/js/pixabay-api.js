8635b3c67b5988202d17acb26950cc91a28bddea
sashok1987nik <sashok1987nk@ukr.net>
2024-04-22 20:37:14 +0200

    upTest

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
 
-const loader = document.querySelector('.loader');
+// const loader = document.querySelector('.loader');
 const KEY = '43440045-443c1624e1412379947e54b30';
 const BASE_URL = 'https://pixabay.com/api/';
 
@@ -9,16 +9,16 @@ const BASE_URL = 'https://pixabay.com/api/';
 
  export function getImages(searchWord) {
 
-    const searchParamObj = {
+    const searchParamObj = new URLSearchParams({
         key: KEY,
         q: searchWord,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-      };
+      });
     const LINK = `${BASE_URL}?${searchParamObj}`;
 
-    loader.style.display = 'block'; /-------появился----/
+    // loader.style.display = 'block'; /-------появился----/
 
     return fetch(LINK)
     .then(response => {
@@ -27,23 +27,5 @@ const BASE_URL = 'https://pixabay.com/api/';
         }
         return response.json();
     })
-    .then(data => {
-
-        loader.style.display = 'none'; /-------исчез----/
-
-        if (data.hits.length === 0) {
-            iziToast.error({
-              message:
-                'Sorry, there are no images matching your search query. Please try again!',
-                backgroundColor: '#ef4040',  
-                titleColor: '#FFFFFF',
-                messageColor: '#FFFFFF',
-                theme: 'dark',
-            });
-        }
-        return data;
-    })
-    .catch(error => {
-        console.log(error);
-    });
+    
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
 import { renderMarcup } from "./js/render-functions.js";
 import { getImages } from "./js/pixabay-api.js";
 
@@ -14,28 +18,105 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
   const form = document.querySelector('.form');
   const gallery = document.querySelector('.gallery');
-  let searchWord = '';
+  const loader = document.querySelector('.loader');
+
+  function showMessageError(textMes) {
+    iziToast.error({
+        iconUrl: iconError,
+        message: textMes,
+        maxWidth: '432px',
+        position: 'topRight',
+        messageSize: 16,
+        backgroundColor: '#ef4040',
+        titleColor: '#FFFFFF',
+        messageColor: '#FFFFFF',
+        theme: 'dark',
+    }); 
+}
+
+  // let searchWord = '';
+
+
+  // form.addEventListener('submit', workSub);
+
+  // function workSub(event) {
+  //   event.preventDefault();
+    
+  //   gallery.innerHTML = '';
+  //   searchWord = form.elements.searchWord.value.trim();
+    
+  //   getImages(searchWord)
+  //   .then(data => {
+  //     const marcup = renderMarcup(data);
+  //     gallery.insertAdjacentHTML('beforeend', marcup);
+
+  //     lightbox.refresh();
+  //   })
+  //   .catch(error => {
+  //     console.error('Error:', error);
+  //   });
+  // form.reset();
+  // }
+function showGallery (searchWord) {
+  if (searchWord) {
+    form.reset();
+    gallery.innerHTML = '';
+    loader.style.display = 'block';
+    getImages(searchWord)
+         .then(data => {
+          console.log(data);
+            //  const arrayImages = data.hits;
+             
+            //  if (arrayImages.length) {
+            //      gallery.innerHTML = renderMarcup(arrayImages);
+            //      lightbox.refresh();
+            //  } else {
+            //      showMessageError(
+            //          'Sorry, there are no images matching your search query. Please try again!');
+            //  }
+         })
+         .catch((error) => showMessageError(error))
+         .finally(() => loader.style.display = 'none');
+ } 
+}
 
 
   form.addEventListener('submit', workSub);
 
   function workSub(event) {
     event.preventDefault();
-    
-    gallery.innerHTML = '';
-    searchWord = form.elements.searchQuery.value.trim();
-    
-    getImages(searchWord)
-    .then(data => {
-      const marcup = renderMarcup(data);
-      container.insertAdjacentHTML('beforeend', marcup);
-
-      lightbox.refresh();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  form.reset();
+    const sarchWord = event.target.elements.searchWord.value;
+    showGallery (sarchWord);
 
   }
+   
+  
 
+
+
+
+
+
+
+
+
+
+//   .then(data => {
+
+//     // loader.style.display = 'none'; /-------исчез----/
+
+//     if (data.hits.length === 0) {
+//         iziToast.error({
+//           message:
+//             'Sorry, there are no images matching your search query. Please try again!',
+//             backgroundColor: '#ef4040',  
+//             titleColor: '#FFFFFF',
+//             messageColor: '#FFFFFF',
+//             theme: 'dark',
+//         });
+//     }
+//     return data;
+// })
+// .catch(error => {
+//     console.log(error);
+// });
